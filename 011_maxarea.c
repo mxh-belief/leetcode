214514673075282e10f418be0d8a77a276ff768c
mxh-belief <1185082848@qq.com>
2018-08-04 16:22:37 +0800

    Update 011_maxarea.c

--- 011_maxarea.c
+++ 011_maxarea.c
@@ -1,1 +1,47 @@
+// int maxArea(int* height, int heightSize) {
+    
+//     int i, j;
+//     int h,maxarea=0,curr_area = 0;
+    
+//     if(heightSize ==0 || heightSize == 1){
+//         return 0 ;
+//     }
+    
+//     for(i=0;i<heightSize;i++){
+//         for(j=0;j<i;j++){
+//             h = height[i]<height[j]?height[i]:height[j];
+//             curr_area = h * (i - j);
+//             maxarea = maxarea<curr_area?curr_area:maxarea;
+//         }
+//     }
+    
+    
+//     return maxarea;
+    
+// }
 
+int maxArea(int* height, int heightSize) {
+    
+    
+   if(heightSize ==0 || heightSize == 1){
+        return 0 ;
+    }
+    
+    int i = 0, j = heightSize -1;
+    int h,maxarea=0,curr_area = 0;
+    
+    while(i<j){
+        h = height[i]<height[j]?height[i]:height[j];  
+        curr_area = h * (j - i);
+        maxarea = maxarea<curr_area?curr_area:maxarea;
+        if(height[i]<height[j]){
+            i++;
+        }else{
+            j--;
+        }
+    }
+    
+    
+    return maxarea;
+    
+}
